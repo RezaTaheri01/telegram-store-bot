b9b15146ffe7d8045ae1ceb55233fef465bc8fc0
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-17 21:31:07 +0330

    Update admin.py

--- telegram_store/users/admin.py
+++ telegram_store/users/admin.py
@@ -5,6 +5,7 @@ from . import models
 # Register your models here.
 class AccountAdmin(admin.ModelAdmin):
     list_display = ['__str__']
+    readonly_fields = ['id']
 
 
 admin.site.register(models.UserData, AccountAdmin)
