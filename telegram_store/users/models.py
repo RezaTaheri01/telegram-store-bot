b9314606c1d7ef20eeeda9f29837e823910908ac
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-17 21:30:51 +0330

    Update models.py

--- telegram_store/users/models.py
+++ telegram_store/users/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 class UserData(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)  # Telegram user ID
-    first_name = models.CharField(max_length=100, null=True)
-    last_name = models.CharField(max_length=100, null=True)
-    mobile_number = models.CharField(max_length=20, null=True)  # null because it may be hidden
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    mobile_number = models.CharField(max_length=20, null=True, blank=True)  # null because it may be hidden
     username = models.CharField(max_length=100, unique=True)
     balance = models.IntegerField(default=0)
 
