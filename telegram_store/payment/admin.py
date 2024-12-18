f33d925a867090425b1a695863ce9a653d41e249
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-18 18:03:27 +0330

    Update admin.py

--- telegram_store/payment/admin.py
+++ telegram_store/payment/admin.py
@@ -5,6 +5,6 @@ from . import models
 # Register your models here.
 @admin.register(models.Transitions)
 class TransitionsAdmin(admin.ModelAdmin):
-    list_display = ('transitions_code', 'amount', 'data_time', 'is_payed')
-    list_filter = ('is_payed', 'data_time')
+    list_display = ('user', 'transitions_code', 'amount', 'paid_time', 'is_paid')
+    list_filter = ('is_paid', 'paid_time')
     search_fields = ('transitions_code',)
