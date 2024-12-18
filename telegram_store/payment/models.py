ed2949d153b779f1256339ee45de9c67149bce64
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-18 18:04:18 +0330

    Update models.py
    fix some mistakes

--- telegram_store/payment/models.py
+++ telegram_store/payment/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from users.models import UserData
+from django.utils import timezone
 
 
 # Create your models here.
 
 class Transitions(models.Model):
+    user = models.ForeignKey(to=UserData, on_delete=models.CASCADE, verbose_name="User", default=686588127)
     transitions_code = models.IntegerField(unique=True, blank=True, null=True)
     amount = models.IntegerField()
-    data_time = models.DateTimeField(null=True, blank=True)
-    is_payed = models.BooleanField(default=False)
+    paid_time = models.DateTimeField(null=True, blank=True)
+    is_paid = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ['-data_time']
+        ordering = ['-paid_time']
         constraints = [
             models.CheckConstraint(
                 check=models.Q(amount__gte=0),
@@ -22,5 +24,6 @@ class Transitions(models.Model):
         verbose_name = "Transition"
 
     def mark_as_paid(self):
-        self.is_payed = True
+        self.is_paid = True
+        self.paid_time = timezone.now()
         self.save()
