bff6e3d8144a46e59cad8cf342781023616488c3
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-23 21:32:13 +0330

    Update admin.py
    add created_date to list_display

--- telegram_store/payment/admin.py
+++ telegram_store/payment/admin.py
@@ -5,6 +5,6 @@ from . import models
 # Register your models here.
 @admin.register(models.Transitions)
 class TransitionsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'transitions_code', 'amount', 'paid_time', 'is_paid')
+    list_display = ('user', 'transitions_code', 'amount', 'paid_time', 'is_paid', 'created_date')
     list_filter = ('is_paid', 'paid_time')
     search_fields = ('transitions_code',)
