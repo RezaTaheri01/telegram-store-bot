727eafd8ca8c96da10b9eae739370afee0aa1aa2
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-27 02:43:17 +0330

    Update models.py

--- telegram_store/users/models.py
+++ telegram_store/users/models.py
@@ -12,7 +12,7 @@ class UserData(models.Model):
     last_name = models.CharField(max_length=64, null=True, blank=True)
     mobile_number = models.CharField(max_length=16, null=True, blank=True)  # null because it may be hidden
     username = models.CharField(max_length=64, unique=True)  # telegram check username to be unique
-    balance = models.IntegerField(max_length=32, default=0)
+    balance = models.IntegerField(default=0)
     language = models.CharField(max_length=8, default='en', choices=LanguageChoices.choices,
                                 verbose_name='Language')
 
