45a070d24e152e07bae90a514df7e285e7f3cc24
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-27 01:08:30 +0330

    Update models.py

--- telegram_store/payment/models.py
+++ telegram_store/payment/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from users.models import UserData
 from django.utils import timezone
+from bot_settings import valid_link_in_seconds
 
 
 # Create your models here.
 
 class Transitions(models.Model):
-    user = models.ForeignKey(to=UserData, on_delete=models.CASCADE, verbose_name="User", default=686588127)
+    user = models.ForeignKey(to=UserData, on_delete=models.CASCADE, verbose_name="User")
     transitions_code = models.IntegerField(unique=True, blank=True, null=True)
-    amount = models.IntegerField()
+    amount = models.IntegerField(max_length=32)
     paid_time = models.DateTimeField(null=True, blank=True)
     is_paid = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now=True)
+    is_delete = models.BooleanField(default=False, blank=True)
 
     class Meta:
         ordering = ['-paid_time']
@@ -30,4 +32,8 @@ class Transitions(models.Model):
         self.save()
 
     def is_expired(self):
-        return (timezone.now() - self.created_date).total_seconds() > 3600  # 1 hour
+        if (timezone.now() - self.created_date).total_seconds() > valid_link_in_seconds:  # 1 hour:
+            self.is_delete = True
+            self.save()
+            return True
+        return False
