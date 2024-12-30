1a78b1b4bbb15e8f7fc242c1301c8b483b6c7168
Reza Taheri <taheri.reza.01@gmail.com>
2024-12-30 23:47:44 +0330

    Update bot_settings.py
    some improvements

--- telegram_store/bot_settings.py
+++ telegram_store/bot_settings.py
@@ -3,6 +3,8 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 categories_in_row = 1
 products_in_row = 1
 valid_link_in_seconds = 3600
+# For more than 2 languages you need to add field to product.model category and product
+# also add your language in bot.py product_categories and products functions
 lang1, lang2 = "en", "fa"
 # aging_limit = 1  # days
 
@@ -13,15 +15,15 @@ bot_link = "https://t.me/{}"  # bot username
 # region Multi language texts
 texts = {
     lang1: {
-        "textError": "Something went wrong.",
+        "textError": "Something went wrong",
         "textStart": "Hello, {}!\nWelcome to Persia shop",  # username
         "textMenu": "Choose from below:",
         "textPriceUnit": "dollar",
-        "textBalance": "Your current balance is {} {}.",  # amount, price unit
+        "textBalance": "Your current balance is {} {}",  # amount, price unit
         "textAmount": "Please enter the amount:",
         "textInvalidAmount": "Invalid input. Please enter a valid number:",
-        "textChargeAccount": "Your account has been successfully charged {} {}.",  # amount, price unit
-        "textPaymentLink": f"Your payment link is ready and it's valid for {valid_link_in_seconds // 3600} hour.",
+        "textChargeAccount": "Your account has been successfully charged {} {}",  # amount, price unit
+        "textPaymentLink": f"Your payment link is ready and it's valid for {valid_link_in_seconds // 3600} hour",
         "textNoTransaction": "No transactions were found",
         "textTransaction": "Here are your last 5 transactions:\n\n",
         "textAccountMenu": "Hello, {}! How can I assist you today?",  # username
@@ -33,12 +35,12 @@ texts = {
         "textInvalidCategory": "Invalid category ID",
         "textNoProductFound": "No product found for this category",
         "textBackButton": "Back",
-        "textInvalidProduct": "Invalid product ID! Please try again.",
+        "textInvalidProduct": "Invalid product ID! Please try again",
         "textProductList": "{} products",  # category name
-        "textProductSoldOut": "This product is sold out or no longer available.",
-        "textPurchaseBill": "The {} costs {} {}.",  # product name, price, price unit
+        "textProductSoldOut": "This product is sold out or no longer available",
+        "textPurchaseBill": "The {} costs {} {}",  # product name, price, price unit
         "textNotEnoughMoney": "Insufficient funds",
-        "textInvalidPaymentAmount": "Invalid payment amount! Please try again.",
+        "textInvalidPaymentAmount": "Invalid payment amount! Please try again",
         "textProductDetail": "Successful\n{}",  # product detail
         "textTransactionDetail": "Amount: {} {}\nDate: {}\n\n",  # amount, priceUnit, datetime
         # Button texts
@@ -53,14 +55,14 @@ texts = {
         "buttonLanguage": lang2,  # don't change this
     },
     lang2: {
-        "textError": "مشکلی پیش آمده است.",
-        "textStart": "سلام، {}!\nبه فروشگاه پرشیا خوش آمدید",  # username
+        "textError": "مشکلی پیش آمده است",
+        "textStart": "سلام {}\nبه فروشگاه پرشیا خوش آمدید",  # username
         "textMenu": "از گزینه‌های زیر انتخاب کنید:",
         "textPriceUnit": "دلار",
-        "textBalance": "موجودی فعلی شما {} {} است.",  # amount, price unit
+        "textBalance": "موجودی فعلی شما {} {} است",  # amount, price unit
         "textAmount": "لطفاً مقدار را وارد کنید:",
         "textInvalidAmount": "ورودی نامعتبر است. لطفاً یک عدد معتبر وارد کنید:",
-        "textChargeAccount": "حساب شما با موفقیت به مقدار {} {} شارژ شد.",  # amount, price unit
+        "textChargeAccount": "حساب شما با موفقیت به مقدار {} {} شارژ شد",  # amount, price unit
         "textPaymentLink": f"لینک پرداخت شما آماده است و برای {valid_link_in_seconds // 3600} ساعت معتبر است.",
         "textNoTransaction": "هیچ تراکنشی یافت نشد",
         "textTransaction": "در اینجا ۵ تراکنش آخر شما آمده است:\n\n",
@@ -73,13 +75,13 @@ texts = {
         "textInvalidCategory": "شناسه دسته‌بندی نامعتبر است",
         "textNoProductFound": "محصولی برای این دسته‌بندی یافت نشد",
         "textBackButton": "بازگشت",
-        "textInvalidProduct": "شناسه محصول نامعتبر است! لطفاً دوباره امتحان کنید.",
-        "textProductList": "{} محصولات",  # category name
-        "textProductSoldOut": "این محصول فروخته شده یا دیگر موجود نیست.",
-        "textPurchaseBill": "{} به قیمت {} {}.",  # product name, price, price unit
+        "textInvalidProduct": "شناسه محصول نامعتبر است! لطفاً دوباره امتحان کنید",
+        "textProductList": "محصولات {}",  # category name
+        "textProductSoldOut": "این محصول فروخته شده یا دیگر موجود نیست",
+        "textPurchaseBill": "{} به قیمت {} {}",  # product name, price, price unit
         "textNotEnoughMoney": "موجودی کافی نیست",
-        "textInvalidPaymentAmount": "مقدار پرداخت نامعتبر است! لطفاً دوباره امتحان کنید.",
-        "textProductDetail": "موفقیت‌آمیز\n{}",  # product detail
+        "textInvalidPaymentAmount": "مقدار پرداخت نامعتبر است! لطفاً دوباره امتحان کنید",
+        "textProductDetail": "موفقیت ‌آمیز\n{}",  # product detail
         "textTransactionDetail": "مقدار: {} {}\nتاریخ: {}\n\n",  # amount, priceUnit, datetime
         # Button texts
         "buttonAccount": "منوی اکانت",
