b049f060f6f75e2c4a0feaed056575a73c61fef0
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-02 01:01:13 +0330

    Update models.py
    username can be null
    
    if username not accessible use first and last name

--- telegram_store/users/models.py
+++ telegram_store/users/models.py
@@ -11,13 +11,15 @@ class UserData(models.Model):
     first_name = models.CharField(max_length=64, null=True, blank=True)
     last_name = models.CharField(max_length=64, null=True, blank=True)
     mobile_number = models.CharField(max_length=16, null=True, blank=True)  # null because it may be hidden
-    username = models.CharField(max_length=64, unique=True)  # telegram check username to be unique
+    username = models.CharField(max_length=64, null=True, blank=True)  # telegram check username to be unique
     balance = models.IntegerField(default=0)
     language = models.CharField(max_length=8, default='en', choices=LanguageChoices.choices,
                                 verbose_name='Language')
 
     def __str__(self):
-        return f"{self.username}"
+        if self.username:
+            return f"{self.username}"
+        return f"{self.first_name or ''} {self.last_name or ''}".strip()
 
     class Meta:
         verbose_name = "User Data"
