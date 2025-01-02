ad934a291d8450f892d63df3ed717950ef6d616d
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-02 23:51:43 +0330

    Update bot_settings.py
    default language is base on lang1 on bot_settings.py

--- telegram_store/users/models.py
+++ telegram_store/users/models.py
@@ -1,19 +1,16 @@
 from django.db import models
+from bot_settings import lang1
 
 
 # Create your models here.
 class UserData(models.Model):
-    # class LanguageChoices(models.TextChoices):
-    #     english = 'en',
-    #     farsi = 'fa'
-
     id = models.IntegerField(primary_key=True, unique=True)  # Telegram user ID
     first_name = models.CharField(max_length=64, null=True, blank=True)
     last_name = models.CharField(max_length=64, null=True, blank=True)
     mobile_number = models.CharField(max_length=16, null=True, blank=True)  # null because it may be hidden
     username = models.CharField(max_length=64, null=True, blank=True)  # telegram check username to be unique
     balance = models.IntegerField(default=0)
-    language = models.CharField(max_length=8, default='en', verbose_name='Language')
+    language = models.CharField(max_length=8, default=lang1, verbose_name='Language')
 
     def __str__(self):
         # if username not founded using full name
