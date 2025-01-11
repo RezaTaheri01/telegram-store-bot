8b088b61b0b1773da2aea709c72c732ad79e8250
Reza Taheri <taheri.reza.01@gmail.com>
2025-01-12 00:43:08 +0330

    Update settings.py
    cast=bool required to recognize it as boolean

--- telegram_store/telegram_store/settings.py
+++ telegram_store/telegram_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", default='django-insecure-$kp!7e*2sv#%i%=qq(-#pspemkli#ruf_5i04(2q+eeoae_+2h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
