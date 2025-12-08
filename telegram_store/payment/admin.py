5bb9c1629175d369a1d73a4965f4a4fd7f606ef6
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-12-08 16:57:21 +0330

    Update payment model & admin
    - Add `last_hash` field to store the last processed transaction hash.
    - Decrease max length of `last_hash` to 128 characters.
    - Remove `atomic_failed` field, as failed transactions are no longer tracked.
    - Adjust default values and string representation accordingly.

--- telegram_store/payment/admin.py
+++ telegram_store/payment/admin.py
@@ -4,8 +4,8 @@ from . import models
 
 @admin.register(models.Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = ("user", "amount", "paid_time", "is_delete", "atomic_failed")
-    list_filter = ("paid_time", "is_delete", "atomic_failed")
+    list_display = ("user", "amount", "paid_time", "is_delete")
+    list_filter = ("paid_time", "is_delete")
     readonly_fields = ("user",
                        "amount",
                        "comment",
@@ -13,11 +13,10 @@ class TransactionAdmin(admin.ModelAdmin):
                        "price_per_ton",
                        "price_currency",
                        "paid_time",
-                       "atomic_failed",
                        "lt"
                        )
     search_fields = ("user__id", "user__username", "tx_id", "comment", "lt")
 
 @admin.register(models.TonCursor)
 class TonCursorAdmin(admin.ModelAdmin):
-   readonly_fields = ("key", "last_lt")
+   readonly_fields = ("key",)
